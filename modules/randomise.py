65f96769deca7c677cf1da9212bcc09e2aa6c4d7
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-07-08 16:59:16 +0100

    chore: clean up practice scripts

--- modules/randomise.py
+++
@@ -1,7 +0,0 @@
-from random import seed
-from random import random
-
-seed(1)
-
-def my_function():
-    return random()
--- tests/test_world.py
+++
@@ -1,6 +0,0 @@
-import path_helper
-
-from func import func
-
-def test_answer():
-    assert func(3) == 5
