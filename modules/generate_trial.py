d9f578f0dd64d0c91a6645d289313cf10646a23c
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-07-08 16:16:40 +0100

    bug freeze: unit test goes into infinite loop

--- modules/generate_trial.py
+++ modules/generate_trial.py
@@ -1,18 +1,41 @@
-import random
+from random import uniform
+from math import floor
+from functools import reduce
 
-def generate_trial(size, food_proportion):
-    return [
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+def generate_trial(food_proportion):
+    trial = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
-    # trial[random.uniform(0, 10)][random.uniform(0, 10)] = 1
-    # return trial
+    for i in range(int(food_proportion * 100)):
+        x = random_0_9()
+        print('x: ', x)
+        y = random_0_9()
+        print('y: ', y)
+        while trial[y][x] == 1:
+            print('x inside while: ', x)
+            x = random_0_9()
+            print('y inside while: ', y)
+            y = random_0_9()
+        trial[y][x] = 1
     
+    return trial
+
+def random_0_9():
+    return floor(uniform(0, 10))
+
+# trial = generate_trial(0.1)
+# print(trial)
+
+# total_food = 0
+# for x in trial:
+#     total_food += reduce(lambda a, b: a + b, x)
+# print(total_food)
--- tests/test_generate_trial.py
+++ tests/test_generate_trial.py
@@ -1,12 +1,11 @@
 import path_helper
 import mock_random
 
-from generate_trial import generate_trial
+import generate_trial
 
-def test_answer(mocker):
-    # mocker.patch.object(generate_trial, "random.uniform", mock_random)
-    assert generate_trial(10, 0.1) ==  [
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+def test_generate_trial_with_10_percent_food(mocker):
+    mocker.patch.object(generate_trial, "random_0_9", mock_random.cycler_0_9)
+    assert generate_trial.generate_trial(0.1) ==  [
         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
@@ -15,5 +14,21 @@ def test_answer(mocker):
         [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
+
+# def test_generate_trial_with_5_percent_food(mocker):
+#     mocker.patch.object(generate_trial, "random_0_9", mock_random.cycler_0_9)
+#     assert generate_trial.generate_trial(0.05) ==  [
+#         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+#         [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+#         [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+#         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+#         [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#         [0, 0, 0, 0, 0, 0, 7, 0, 0, 0],
+#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#     ]
