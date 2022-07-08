7e47a34ab0055ac95cb38a5c849e5a6bb61d21f6
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-07-08 17:10:47 +0100

    refactor: extract private functions in generate_trial module

--- modules/generate_trial.py
+++ modules/generate_trial.py
@@ -4,18 +4,30 @@ from functools import reduce
 import numpy as np
 
 def generate_trial(size, food_proportion):
-    if food_proportion > 1 or food_proportion < 0:
-        raise Exception('food_proportion must be between 0 and 1') 
+    __guard_invalid_food_proportion(food_proportion)
     trial = np.zeros((size, size))
-    for i in range(int(food_proportion * 100)):
-        x = random_0_9()
-        y = random_0_9()
-        while trial[y][x] == 1:
-            x = random_0_9()
-            y = random_0_9()
-        trial[y][x] = 1
-    
+    trial = __populate_food(trial, food_proportion)
     return trial
 
-def random_0_9():
-    return floor(uniform(0, 10))
+def __guard_invalid_food_proportion(food_proportion):
+    if food_proportion > 1 or food_proportion < 0:
+        raise Exception('food_proportion must be between 0 and 1') 
+
+def __populate_food(trial, food_proportion):
+    size = len(trial)
+    for i in range(int(food_proportion * 100)):
+        x, y = __select_random_cell(size)
+        while trial[y][x] == 1:
+            x, y = __select_random_cell(size)
+        trial[y][x] = 1
+    return trial
+
+def __select_random_cell(size):
+    x = __select_random_index(size)
+    y = __select_random_index(size)
+    return x, y
+
+
+def __select_random_index(size):
+    return floor(uniform(0, size))
+    
