1ff0287e7dc16ec45aa0354c58b98ad714ede218
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-07-08 16:47:26 +0100

    test & implement: generate_trial generalised for any size, food_proportion

--- modules/generate_trial.py
+++ modules/generate_trial.py
@@ -1,29 +1,15 @@
 from random import uniform
 from math import floor
 from functools import reduce
+import numpy as np
 
-def generate_trial(food_proportion):
-    trial = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ]
+def generate_trial(size, food_proportion):
+    trial = np.zeros((size, size))
     for i in range(int(food_proportion * 100)):
         x = random_0_9()
-        print('x: ', x)
         y = random_0_9()
-        print('y: ', y)
         while trial[y][x] == 1:
-            print('x inside while: ', x)
             x = random_0_9()
-            print('y inside while: ', y)
             y = random_0_9()
         trial[y][x] = 1
     
@@ -31,11 +17,3 @@ def generate_trial(food_proportion):
 
 def random_0_9():
     return floor(uniform(0, 10))
-
-# trial = generate_trial(0.1)
-# print(trial)
-
-# total_food = 0
-# for x in trial:
-#     total_food += reduce(lambda a, b: a + b, x)
-# print(total_food)
--- tests/test_generate_trial.py
+++ tests/test_generate_trial.py
@@ -1,34 +1,67 @@
+from functools import reduce
 import path_helper
-import mock_random
 
-import generate_trial
+from generate_trial import generate_trial
 
-def test_generate_trial_with_10_percent_food(mocker):
-    mocker.patch.object(generate_trial, "random_0_9", mock_random.cycler_0_9)
-    assert generate_trial.generate_trial(0.1) ==  [
-        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ]
+def test_generate_trial_return_has_width_and_height_equal_to_size():
+    size = 10
+    food_proportion = 0
+    trial = generate_trial(size, food_proportion)
+    assert len(trial) == size
 
-# def test_generate_trial_with_5_percent_food(mocker):
-#     mocker.patch.object(generate_trial, "random_0_9", mock_random.cycler_0_9)
-#     assert generate_trial.generate_trial(0.05) ==  [
-#         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 7, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#     ]
+    size = 20
+    food_proportion = 0
+    trial = generate_trial(size, food_proportion)
+    assert len(trial) == size
+
+
+def test_generate_trial_returns_cells_only_0s_and_1s():
+    size = 10
+    food_proportion = 0.1
+    trial = generate_trial(size, food_proportion)
+    for i in range(size):
+        for j in range(size):
+            assert (trial[i][j] == 1 or trial[i][j] == 0)
+
+    size = 10
+    food_proportion = 0.5
+    trial = generate_trial(size, food_proportion)
+    for i in range(size):
+        for j in range(size):
+            assert (trial[i][j] == 1 or trial[i][j] == 0)
+
+    size = 20
+    food_proportion = 0.5
+    trial = generate_trial(size, food_proportion)
+    for i in range(size):
+        for j in range(size):
+            assert (trial[i][j] == 1 or trial[i][j] == 0)
+
+def test_generate_trial_returns_food_proportion_cells_of_1s():
+    size = 10
+    food_proportion = 0.1
+    num_cells = food_proportion * size * size
+    trial = generate_trial(size, food_proportion)
+    total_food = 0
+    for x in trial:
+        total_food += reduce(lambda a, b: a + b, x)
+    assert total_food == num_cells
+    
+    size = 10
+    food_proportion = 0.2
+    num_cells = food_proportion * size * size
+    trial = generate_trial(size, food_proportion)
+    total_food = 0
+    for x in trial:
+        total_food += reduce(lambda a, b: a + b, x)
+    assert total_food == num_cells
+
+    size = 10
+    food_proportion = 0.7
+    num_cells = food_proportion * size * size
+    trial = generate_trial(size, food_proportion)
+    total_food = 0
+    for x in trial:
+        total_food += reduce(lambda a, b: a + b, x)
+    assert total_food == num_cells
+    
