dd745180bd23edc47003cfb5a279db06db32059d
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-07-11 17:05:27 +0100

    test & implement: slow/expensive create_genome. Explore base x incrementer where x is number of features

--- modules/create_genome.py
+++ modules/create_genome.py
@@ -1,4 +1,15 @@
+from random import uniform
+from math import floor
+
+
 def create_genome(spaces, features):
     genome = []
-    genome.extend(range(features ** spaces))
+    while len(genome) < features ** spaces:
+        position = ''
+        for x in range(5):
+            position += str(floor(uniform(0, 3)))
+        print(position)
+        if position not in genome:
+            genome.append(position)
+        print(genome)
     return genome
--- tests/test_create_genome.py
+++ tests/test_create_genome.py
@@ -23,7 +23,7 @@ def test_create_genome_return_list_w_num_els_equal_to_features_to_power_of_space
     features = 0
     genome = create_genome(spaces, features)
     assert len(genome) == features ** spaces
-   
+
 
 def test_create_genome_return_list_of_unique_elements():
     spaces = 5
@@ -52,3 +52,11 @@ def test_create_genome_return_list_of_unique_elements():
         for y in genome:
             assert x != y
         genome.append(x)
+
+
+def test_create_genome_return_list_of_elements_each_a_string_of_spaces_number_of_chars():
+    spaces = 5
+    features = 3
+    genome = create_genome(spaces, features)
+    for x in genome:
+        assert len(x) == spaces
