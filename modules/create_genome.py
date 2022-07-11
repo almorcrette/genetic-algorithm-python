5db40aa7409332f477a70fb84d032de283fd8bb3
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-07-11 17:21:35 +0100

    test & implement: create_genome with positions each with spaces number of characters

--- modules/create_genome.py
+++ modules/create_genome.py
@@ -1,15 +1,12 @@
-from random import uniform
-from math import floor
+import itertools
 
 
 def create_genome(spaces, features):
     genome = []
-    while len(genome) < features ** spaces:
-        position = ''
-        for x in range(5):
-            position += str(floor(uniform(0, 3)))
-        print(position)
-        if position not in genome:
-            genome.append(position)
-        print(genome)
+    digits = ''
+    for x in range(features):
+        digits += str(x)
+    for num in itertools.product(digits, repeat=spaces):
+        s = ''.join(num)
+        genome.append(s)
     return genome
