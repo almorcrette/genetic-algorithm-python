553071c28c90f676159c5a0dfcc5bc443dc15f00
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-09-03 16:22:51 +0100

    feat: create_world with number of trials, trial_generator, and generator_args

--- modules/create_world.py
+++ modules/create_world.py
@@ -1,2 +1,5 @@
-def create_world(num_trial, trial_generator):
-    return list(range(num_trial))
+def create_world(num_trials, trial_generator, *generator_args):
+    world = []
+    for _ in range(num_trials):
+        world.append(trial_generator(*generator_args))
+    return world
--- tests/test_create_world.py
+++ tests/test_create_world.py
@@ -7,15 +7,36 @@ from generate_trial import generate_trial
 def test_create_world_returns_list_length_num_trials():
     num_trials = 10
     trial_generator = generate_trial
-    world = create_world(num_trials, trial_generator)
+    generator_arg_size = 10
+    generator_arg_food_proportion = 0.5
+    world = create_world(
+        num_trials,
+        trial_generator,
+        generator_arg_size,
+        generator_arg_food_proportion
+        )
     assert len(world) == num_trials
 
     num_trials = 5
     trial_generator = generate_trial
-    world = create_world(num_trials, trial_generator)
+    generator_arg_size = 10
+    generator_arg_food_proportion = 0.5
+    world = create_world(
+        num_trials,
+        trial_generator,
+        generator_arg_size,
+        generator_arg_food_proportion
+        )
     assert len(world) == num_trials
 
     num_trials = 2
     trial_generator = generate_trial
-    world = create_world(num_trials, trial_generator)
+    generator_arg_size = 10
+    generator_arg_food_proportion = 0.5
+    world = create_world(
+        num_trials,
+        trial_generator,
+        generator_arg_size,
+        generator_arg_food_proportion
+        )
     assert len(world) == num_trials
