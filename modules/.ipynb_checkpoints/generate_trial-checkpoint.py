91ca317193acb5722a02e7d01d1c25556e6d785f
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-09-03 15:50:46 +0100

    test: add test for create_genome confirming elements only have admissisble characters

---
+++ modules/.ipynb_checkpoints/generate_trial-checkpoint.py
@@ -0,0 +1,35 @@
+from random import uniform
+from math import floor
+import numpy as np
+
+
+def generate_trial(size, food_proportion):
+    __guard_invalid_food_proportion(food_proportion)
+    trial = np.zeros((size, size))
+    trial = __populate_food(trial, food_proportion)
+    return trial
+
+
+def __guard_invalid_food_proportion(food_proportion):
+    if food_proportion > 1 or food_proportion < 0:
+        raise Exception('food_proportion must be between 0 and 1')
+
+
+def __populate_food(trial, food_proportion):
+    size = len(trial)
+    for i in range(int(food_proportion * 100)):
+        x, y = __select_random_cell(size)
+        while trial[y][x] == 1:
+            x, y = __select_random_cell(size)
+        trial[y][x] = 1
+    return trial
+
+
+def __select_random_cell(size):
+    x = __select_random_index(size)
+    y = __select_random_index(size)
+    return x, y
+
+
+def __select_random_index(size):
+    return floor(uniform(0, size))
--- tests/test_create_genome.py
+++ tests/test_create_genome.py
@@ -3,7 +3,7 @@ import path_helper
 from create_genome import create_genome
 
 
-def test_create_genome_return_list_w_num_els_equal_to_features_to_power_of_spaces():
+def test_create_genome_returns_list_with_number_elements_equal_to_features_to_power_of_spaces():
     spaces = 5
     features = 3
     genome = create_genome(spaces, features)
@@ -25,7 +25,7 @@ def test_create_genome_return_list_w_num_els_equal_to_features_to_power_of_space
     assert len(genome) == features ** spaces
 
 
-def test_create_genome_return_list_of_unique_elements():
+def test_create_genome_returns_list_of_unique_elements():
     spaces = 5
     features = 3
     genome = create_genome(spaces, features)
@@ -54,11 +54,48 @@ def test_create_genome_return_list_of_unique_elements():
         genome.append(x)
 
 
-def test_create_genome_return_list_of_elements_each_a_string_of_spaces_number_of_chars():
+def test_create_genome_returns_list_of_strings_of_length_spaces():
     spaces = 5
     features = 3
     genome = create_genome(spaces, features)
     for x in genome:
         assert len(x) == spaces
 
+    spaces = 5
+    features = 2
+    genome = create_genome(spaces, features)
+    for x in genome:
+        assert len(x) == spaces
+
+    spaces = 3
+    features = 3
+    genome = create_genome(spaces, features)
+    for x in genome:
+        assert len(x) == spaces
+
 # Final test which checks that every char is one of the digits in the range of features
+
+def test_create_genome_returns_list_of_strings_whose_characters_are_only_digits_in_range_of_features():
+    spaces = 5
+    features = 3
+    feature_options = list(range(features))
+    genome = create_genome(spaces, features)
+    for x in genome:
+        for char in x:
+            assert int(char) in feature_options
+
+    spaces = 5
+    features = 2
+    feature_options = list(range(features))
+    genome = create_genome(spaces, features)
+    for x in genome:
+        for char in x:
+            assert int(char) in feature_options
+
+    spaces = 3
+    features = 3
+    feature_options = list(range(features))
+    genome = create_genome(spaces, features)
+    for x in genome:
+        for char in x:
+            assert int(char) in feature_options
