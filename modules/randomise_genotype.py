f39c733f77393b2d7c1a6a60cbc211cff4e91fea
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-09-04 11:24:55 +0100

    feat: randomise_genotype(num_spaces, num_features, num_responses)

--- modules/randomise_genotype.py
+++ modules/randomise_genotype.py
@@ -1,5 +1,8 @@
+from random import choice
+
 def randomise_genotype(num_spaces, num_features, num_responses):
+    response_options = list(range(num_responses))
     genotype = ""
     for _ in range(num_features ** num_spaces):
-        genotype += "6"
+        genotype += str(choice(response_options))
     return genotype
