017c438cb8a457acba95deef91804ecb0a551e72
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-09-04 10:22:43 +0100

    test: create_world returns list of returns of trial generator calls

--- tests/test_create_world.py
+++ tests/test_create_world.py
@@ -1,5 +1,3 @@
-import path_helper
-
 from create_world import create_world
 from generate_trial import generate_trial
 
@@ -41,4 +39,31 @@ def test_create_world_returns_list_length_num_trials():
         )
     assert len(world) == num_trials
 
-# Missing tests for trial_generator and generator_args
+
+def mock_generate_trial_returning_count():
+    val = [0]
+
+    def inc():
+        val[0] += 1
+        return val[0]
+    return inc
+
+
+def test_create_world_returns_list_of_returns_of_trial_generator_calls():
+    num_trials = 3
+    trial_generator = mock_generate_trial_returning_count()
+    world = create_world(
+        num_trials,
+        trial_generator
+        )
+    print(world)
+    assert world == [1, 2, 3]
+
+    num_trials = 5
+    trial_generator = mock_generate_trial_returning_count()
+    world = create_world(
+        num_trials,
+        trial_generator
+        )
+    print(world)
+    assert world == [1, 2, 3, 4, 5]
